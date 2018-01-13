b88965e173b616e5d348f37cee0b35c37196fc7b
Hayk Sargsyan <hayk157@mail.ru>
2018-01-14 00:58:09 +0400

    identity section created, still to connect the project to azure and program will be done

--- SportsStore/SportsStore/Controllers/AdminController.cs
+++ SportsStore/SportsStore/Controllers/AdminController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 
 
 
 namespace SportsStore.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         private IProductRepository repository;
