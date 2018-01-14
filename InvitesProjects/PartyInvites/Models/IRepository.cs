b509b310854b9b855fb3bc5c320ad5ac551c0c7e
Hayk Sargsyan <hayk157@mail.ru>
2018-01-14 15:02:01 +0400

    VS code project PartyInvite with SQLite database added

---
+++ InvitesProjects/PartyInvites/Models/IRepository.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+
+namespace PartyInvites.Models {
+
+ public interface IRepository {
+
+ IEnumerable<GuestResponse> Responses {get; }
+
+ void AddResponse(GuestResponse response);
+
+ }
+}
